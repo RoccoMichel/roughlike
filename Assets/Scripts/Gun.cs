b287523b778af03fff14160c6f71088c752ea5d4
BabyDigger <Charlie.stalhammar@gmail.com>
2024-11-04 18:20:37 +0100

    added muzzel flash

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,13 +8,22 @@ public class Gun : MonoBehaviour
     public float range;
     public float fireRate;
     bool hasFired = false;
+
     public Transform tip;
+    public GameObject muzzelFlach;
+
     public LayerMask enemy;
 
     public void Shoot()
     {
         if (!hasFired)
         {
+            Vector3 rot = new Vector3(0, 0, 0);
+            rot.x = -transform.eulerAngles.z;
+            rot.y = 90;
+
+            Instantiate(muzzelFlach, tip.position, Quaternion.Euler(rot));
+
             ammo -= 1;
             RaycastHit2D hit = Physics2D.Raycast(tip.position, transform.right, range, enemy);
 
