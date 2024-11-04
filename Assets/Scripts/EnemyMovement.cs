1fb2ab7c2a250e08d5e99fb1ba9f77d3f944c4ca
Bolt_Bug <Charlie.stalhammar@gmail.com>
2024-11-04 12:45:11 +0100

    began on the enemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [Header("AI")]
     public AIDestinationSetter ads;
     public AIPath path;
     public Transform player;
     public float stoppingDistens = 1;
+    bool canMove = true;
 
     public LayerMask Player;
 
@@ -14,19 +16,37 @@ public class EnemyMovement : MonoBehaviour
     {
         if (ads.target == null) ads.target = player;
 
+        //Moves the agent to the player if the distens is more then the stopping distens
         float dist = Vector2.Distance(player.position, transform.position);
-        if (dist > stoppingDistens)
+        if (dist > stoppingDistens && canMove)
             path.canMove = true;
         else
         {
             path.canMove = false;
 
-            Vector3 dir = (transform.position - player.position).normalized;
-            RaycastHit hit;
-            if(Physics.Raycast(transform.position, dir, out hit, 10, Player))
+            //Shoting
+            Vector3 dir = -(transform.position - player.position).normalized;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 10, Player);
+            if(hit)
             {
-
+                //Shoot
             }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "EnemyStop")
+        {
+            canMove = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "EnemyStop")
+        {
+            canMove = true;
         }
     }
 }
