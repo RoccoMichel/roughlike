db98eae61439799a6abfc1ef06f5ed2424f5e52e
NinjaElite24 <156318142+NinjaElite24@users.noreply.github.com>
2024-11-04 14:35:54 +0100

    main menu
    main menu started wahooo

---
+++ Assets/Scripts/SceneLogicScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLogicScript : MonoBehaviour
+{
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void LoadNextInBuild()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadPreviousInBuild()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    public void LoadSceneByNumber(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
